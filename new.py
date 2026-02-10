b462710f2e0eb497cb1fdb2193d41c3ce835b2e9
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-02-10 19:39:49 +0530

    practice

--- new.py
+++ new.py
@@ -1,19 +1,36 @@
+# import random
+
+# number = random.randint(1, 100)
+# attempts = 0
+
+# print("🎮 Welcome to the Number Guessing Game!")
+# print("Guess a number between 1 and 100")
+
+# while True:
+#     guess = int(input("Enter your guess: "))
+#     attempts += 1
+
+#     if guess < number:
+#         print("Too low! Try again 🔽")
+#     elif guess > number:
+#         print("Too high! Try again 🔼")
+#     else:
+#         print(f"🎉 Correct! You guessed it in {attempts} attempts.")
+#         break
+
+
+# tasks = []
+
+# while True:
+#     task = input("Enter task (or 'exit' to stop): ")
+#     if task == "exit":
+#         break
+#     tasks.append(task)
+
+# print("\nYour To-Do List:")
+# for t in tasks:
+#     print("-", t)
 import random
 
-number = random.randint(1, 100)
-attempts = 0
-
-print("🎮 Welcome to the Number Guessing Game!")
-print("Guess a number between 1 and 100")
-
-while True:
-    guess = int(input("Enter your guess: "))
-    attempts += 1
-
-    if guess < number:
-        print("Too low! Try again 🔽")
-    elif guess > number:
-        print("Too high! Try again 🔼")
-    else:
-        print(f"🎉 Correct! You guessed it in {attempts} attempts.")
-        break
+print("Rolling the dice...")
+print("You got:", random.randint(1, 6))

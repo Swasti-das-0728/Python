6b235ecf1650d1b8064c4400be52b56c3611716e
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-02-11 21:45:35 +0530

    add

--- practice.py
+++ practice.py
@@ -1,13 +1,24 @@
 
-# sentence = input("Enter a sentence: ")
-# words = sentence.split()
-# print("Total words:", len(words))
+# # sentence = input("Enter a sentence: ")
+# # words = sentence.split()
+# # print("Total words:", len(words))
 
 
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = "".join(random.choice(characters) for _ in range(length))
+
+print("Generated Password:", password)

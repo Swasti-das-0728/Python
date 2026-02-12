9c963bea493b1f43ec7b510717995329d592154d
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-02-12 20:12:34 +0530

    add new

--- practice.py
+++ practice.py
@@ -1,24 +1,35 @@
 
-# # sentence = input("Enter a sentence: ")
-# # words = sentence.split()
-# # print("Total words:", len(words))
+# # # sentence = input("Enter a sentence: ")
+# # # words = sentence.split()
+# # # print("Total words:", len(words))
 
 
 
-# num = int(input("Enter a number: "))
+# # num = int(input("Enter a number: "))
 
-# if num % 2 == 0:
-#     print("Even number")
-# else:
-#     print("Odd number")
+# # if num % 2 == 0:
+# #     print("Even number")
+# # else:
+# #     print("Odd number")
 
 
-import random
-import string
+# import random
+# import string
 
-length = int(input("Enter password length: "))
+# length = int(input("Enter password length: "))
 
-characters = string.ascii_letters + string.digits + string.punctuation
-password = "".join(random.choice(characters) for _ in range(length))
+# characters = string.ascii_letters + string.digits + string.punctuation
+# password = "".join(random.choice(characters) for _ in range(length))
 
-print("Generated Password:", password)
+# print("Generated Password:", password)
+
+import time
+
+seconds = int(input("Enter time in seconds: "))
+
+while seconds > 0:
+    print(seconds)
+    time.sleep(1)
+    seconds -= 1
+
+print("Time's up! ⏰")

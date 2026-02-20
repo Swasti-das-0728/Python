ad2e2cb4c7e3cdc2b6f35ca164be5fd049c6ee9f
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-02-20 22:47:27 +0530

    add

---
+++ DSA.py/inputarr.py
@@ -0,0 +1,16 @@
+from array import *
+# arr = array('i',[])
+
+# # n = int(input('enter the numbers'))
+# # for i in range(0,n):
+# #     arr.append(int(input('enter the number')))
+
+# # for x in arr:
+# #     print(x, end=" ")
+
+
+# arr = array('i', [1,2,3,4,5,6,7,8,9,10])
+# i = arr.index(9)
+# print(i)
+
+
---
+++ DSA.py/numpy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+hi = np.array([1, 2, 3])
+
+for x in hi:
+    print(x, end=" ")

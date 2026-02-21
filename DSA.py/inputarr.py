783350c5c7189ceadb010fdcd76ab673e91647d9
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-02-21 16:33:31 +0530

    add

--- DSA.py/inputarr.py
+++ DSA.py/inputarr.py
@@ -1,16 +1,19 @@
-from array import *
-# arr = array('i',[])
+# from array import *
+# # arr = array('i',[])
 
-# # n = int(input('enter the numbers'))
-# # for i in range(0,n):
-# #     arr.append(int(input('enter the number')))
+# # # n = int(input('enter the numbers'))
+# # # for i in range(0,n):
+# # #     arr.append(int(input('enter the number')))
 
-# # for x in arr:
-# #     print(x, end=" ")
-
-
-# arr = array('i', [1,2,3,4,5,6,7,8,9,10])
-# i = arr.index(9)
-# print(i)
+# # # for x in arr:
+# # #     print(x, end=" ")
 
 
+# # arr = array('i', [1,2,3,4,5,6,7,8,9,10])
+# # i = arr.index(9)
+# # print(i)
+
+name = "swasti"
+age = 43
+price = 25.98
+print(price)

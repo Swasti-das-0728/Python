b5dda14842c4f7be9dc9dbadbc44057e83a529b4
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-02-24 22:47:28 +0530

    add

--- DSA.py/2nd.py
+++ DSA.py/2nd.py
@@ -1,10 +1,8 @@
-num = int(input("Enter number: "))
-if num > 1:
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            print("Not Prime")
-            break
-    else:
-        print("Prime")
-else:
-    print("Not Prime")
+from array import *
+array1 = array('i', [3,4,5,6,7,7])
+array1.pop(3)
+array1.remove(3)
+array1.insert(0,9)
+for i in range(0,len(array1)):
+    print(array1[i])
+print(array1)

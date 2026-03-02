52f6c40bd79a1af62adf0a2abe49aea9f9c14066
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-02 20:13:32 +0530

    jj

--- DSA.py/5th.py
+++ DSA.py/5th.py
@@ -1,51 +1,63 @@
-# # def reverse_string(s):
-# #     return s[::-1]
+# # # def reverse_string(s):
+# # #     return s[::-1]
 
-# # print(reverse_string("swasti"))
+# # # print(reverse_string("swasti"))
 
-# def binary_search(arr, target):
-#     left, right = 0, len(arr)-1
+# # def binary_search(arr, target):
+# #     left, right = 0, len(arr)-1
     
-#     while left <= right:
-#         mid = (left + right) // 2
+# #     while left <= right:
+# #         mid = (left + right) // 2
         
-#         if arr[mid] == target:
-#             return mid
-#         elif arr[mid] < target:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
-#     return -1
+# #         if arr[mid] == target:
+# #             return mid
+# #         elif arr[mid] < target:
+# #             left = mid + 1
+# #         else:
+# #             right = mid - 1
+# #     return -1
 
-# print(binary_search([1,2,3,4,5], 4))
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# # print(binary_search([1,2,3,4,5], 4))
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
 
-    def insert(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            return
+#     def insert(self, data):
+#         new_node = Node(data)
+#         if not self.head:
+#             self.head = new_node
+#             return
         
-        temp = self.head
-        while temp.next:
-            temp = temp.next
-        temp.next = new_node
+#         temp = self.head
+#         while temp.next:
+#             temp = temp.next
+#         temp.next = new_node
 
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" -> ")
-            temp = temp.next
+#     def display(self):
+#         temp = self.head
+#         while temp:
+#             print(temp.data, end=" -> ")
+#             temp = temp.next
 
-ll = LinkedList()
-ll.insert(10)
-ll.insert(20)
-ll.insert(30)
-ll.display()
+# ll = LinkedList()
+# ll.insert(10)
+# ll.insert(20)
+# ll.insert(30)
+# ll.display()
+
+def max_subarray_sum(arr, k):
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+    
+    for i in range(k, len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        max_sum = max(max_sum, window_sum)
+    
+    return max_sum
+
+print(max_subarray_sum([2,1,5,1,3,2], 3))

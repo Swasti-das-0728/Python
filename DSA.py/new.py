6fd406b0ba45191069ff475195973be00885181c
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-07 20:08:26 +0530

    add

--- DSA.py/new.py
+++ DSA.py/new.py
@@ -1,16 +1,27 @@
-# def reverse_string(s):
-#     return s[::-1]
+# # def reverse_string(s):
+# #     return s[::-1]
 
-# text = input("Enter a string: ")
-# print("Reversed string:", reverse_string(text))
-stack = []
+# # text = input("Enter a string: ")
+# # print("Reversed string:", reverse_string(text))
+# stack = []
 
-stack.append(10)
-stack.append(20)
-stack.append(30)
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
 
-print("Stack:", stack)
+# print("Stack:", stack)
 
-stack.pop()
+# stack.pop()
 
-print("After pop:", stack)
+# print("After pop:", stack)
+
+
+arr = [10, 45, 2, 99, 23]
+
+largest = arr[0]
+
+for num in arr:
+    if num > largest:
+        largest = num
+
+print("Largest element:", largest)

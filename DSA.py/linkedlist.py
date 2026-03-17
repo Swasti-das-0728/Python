7aad8bbecec2aa36c7aedb1aa17484826cce7f82
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-17 20:08:47 +0530

    add

---
+++ DSA.py/linkedlist.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert at end
+    def insert(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    # Display list
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+# Create Linked List
+ll = LinkedList()
+
+ll.insert(10)
+ll.insert(20)
+ll.insert(30)
+
+ll.display()

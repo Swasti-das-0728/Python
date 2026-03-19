3434b589739eac2a5375c3ffa4cb1d344f07dcb3
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-19 14:54:22 +0530

    add

--- DSA.py/linkedlist.py
+++ DSA.py/linkedlist.py
@@ -59,11 +59,39 @@
 #         carry = sum_val // 10
 
 #     return ''.join(result[::-1])
-arr = [5,3,8,4,2]
+# arr = [5,3,8,4,2]
 
-for i in range(len(arr)):
-    for j in range(len(arr)-i-1):
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+# for i in range(len(arr)):
+#     for j in range(len(arr)-i-1):
+#         if arr[j] > arr[j+1]:
+#             arr[j], arr[j+1] = arr[j+1], arr[j]
 
-print(arr)
+# printkhn(arr);
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+# Example
+arr = [1, 3, 5, 7, 9, 11]
+target = 7
+
+result = binary_search(arr, target
+
+if result != -1:
+    print("Element found at index:", result)
+else:
+    print("Element not found")

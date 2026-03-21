a45b56a2264198745f842b4626be06ab5658bb91
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-21 08:44:49 +0530

    add

--- DSA.py/linkedlist.py
+++ DSA.py/linkedlist.py
@@ -96,44 +96,58 @@
 # else:
 #     print("Element not found")
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
 
-    # Insert at end
-    def insert(self, data):
-        new_node = Node(data)
+#     # Insert at end
+#     def insert(self, data):
+#         new_node = Node(data)
 
-        if self.head is None:
-            self.head = new_node
-            return
+#         if self.head is None:
+#             self.head = new_node
+#             return
 
-        temp = self.head
-        while temp.next:
-            temp = temp.next
+#         temp = self.head
+#         while temp.next:
+#             temp = temp.next
 
-        temp.next = new_node
+#         temp.next = new_node
 
-    # Display list
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" -> ")
-            temp = temp.next
-        print("None")
+#     # Display list
+#     def display(self):
+#         temp = self.head
+#         while temp:
+#             print(temp.data, end=" -> ")
+#             temp = temp.next
+#         print("None")
 
 
-# Create Linked List
-ll = LinkedList()
+# # Create Linked List
+# ll = LinkedList()
 
-ll.insert(10)
-ll.insert(20)
-ll.insert(30)
+# ll.insert(10)
+# ll.insert(20)
+# ll.insert(30)
 
-ll.display()
+# ll.display()
+
+# n = int(input())
+
+# if n % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+a = 5
+b = 10
+
+a, b = b, a
+
+print(a, b)

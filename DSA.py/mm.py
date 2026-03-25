a9543399a0576922db70e8c6dbb22f6711f7bc6e
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-25 20:41:40 +0530

    day streak code

--- DSA.py/mm.py
+++ DSA.py/mm.py
@@ -1,7 +1,12 @@
-arr = [2, 7, 11, 15]
-target = 9
+# arr = [2, 7, 11, 15]
+# target = 9
 
-for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
-        if arr[i] + arr[j] == target:
-            print(i, j)
+# for i in range(len(arr)):
+#     for j in range(i+1, len(arr)):
+#         if arr[i] + arr[j] == target:
+#             print(i, j)
+arr = [1, 2, 2, 3, 4, 4, 5]
+
+unique = list(set(arr))
+
+print(unique)

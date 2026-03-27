8bfd35ef0878a0c98f533999bf16eab968e05be0
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-27 17:14:43 +0530

    Day X: problem name

---
+++ 7th.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+a = 10
+b = 5
+
+print("Add:", add(a, b))
+print("Sub:", sub(a, b))
+print("Mul:", mul(a, b))
+print("Div:", div(a, b))

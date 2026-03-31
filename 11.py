d47161365507fa50a05185e199f0e9b6a0442070
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-03-31 18:38:19 +0530

    Day X: problem name

---
+++ 11.py
@@ -0,0 +1,15 @@
+# Stack implementation
+stack = []
+
+# Push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack:", stack)
+
+# Pop
+print("Popped:", stack.pop())
+
+# Peek
+print("Top element:", stack[-1])

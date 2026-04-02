f8cca1e2e39f635dbf357f99e0d6450abf11e2af
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-04-02 20:39:29 +0530

    Day X: problem name

--- 13.py
+++ 13.py
@@ -1,6 +1,11 @@
-num = int(input("Enter number: "))
+# num = int(input("Enter number: "))
 
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+s = "hello"
+
+print(s.upper())
+print(s.lower())
+print(len(s))

0db28309dd58d060e7fa1510cf6f40a848b7b4cf
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-09 23:41:21 +0530

    add

---
+++ DSA.py/practice.py
@@ -0,0 +1,17 @@
+# arr = [23,45,65,765]
+# print (arr)
+
+
+# arr = list(map(int,input().split()))
+# print(arr)
+# arr = [4,5,6,78,98]
+
+# arr[2] = 43
+# print(arr[0])
+# print(arr[-1])
+
+# print(arr)
+
+arr = [3,5,66,54]
+arr.append(56)
+print(arr)

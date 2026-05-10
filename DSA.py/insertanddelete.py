060bc267834122f2b0212f9528e3ec5b24bd1c14
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-10 20:40:50 +0530

    add

---
+++ DSA.py/insertanddelete.py
@@ -0,0 +1,7 @@
+arr =[4,6,7,8,5,66,5]
+arr.insert(4,4)
+arr.remove(5)
+arr.pop()
+arr1 = arr.copy()
+print(len(arr1))
+print(arr)
--- DSA.py/practice.py
+++ DSA.py/practice.py
@@ -1,17 +1,3 @@
-# arr = [23,45,65,765]
-# print (arr)
-
-
-# arr = list(map(int,input().split()))
-# print(arr)
-# arr = [4,5,6,78,98]
-
-# arr[2] = 43
-# print(arr[0])
-# print(arr[-1])
-
-# print(arr)
-
-arr = [3,5,66,54]
-arr.append(56)
-print(arr)
+arr = list(map(int, input("enter the element for your aarray").split()))
+arr.append(32)
+print (arr)

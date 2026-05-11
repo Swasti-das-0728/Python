83a359fb5bcf44e101e3e5654a0bd8b5e41fc703
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-11 21:12:05 +0530

    add

---
+++ DSA.py/typeofsearch.py
@@ -0,0 +1,11 @@
+# arr = [22,54,65,44]
+# x = 65
+# if x in arr:
+#     print("found")
+# else:
+#     print("not found")
+arr = [34,65,7,543,234,43,53]
+x = 53
+for i in range (len(arr)):
+    if arr[i] == x:
+     print("found at the index", i)

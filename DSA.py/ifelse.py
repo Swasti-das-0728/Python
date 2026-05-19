613f8bbfaa149fa6a2fdc5e081faccc7c72aca30
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-19 22:42:20 +0530

    add

--- DSA.py/ifelse.py
+++ DSA.py/ifelse.py
@@ -1,6 +1,13 @@
-num = int(input("enter the number: "))
+# num = int(input("enter the number: "))
 
-if num > 0:
-    print("Positive")
+# if num > 0:
+#     print("Positive")
+# else:
+#     print("Negative")
+
+age = int(input("enter the age "))
+if age >= 16:
+    print("u can vote")
 else:
-    print("Negative")
+    print("u can not give vote")
+print(age)

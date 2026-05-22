5e0abd86ebf9e9af44b20e584c192dd3fcbb14d3
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-22 23:09:44 +0530

    advance

--- DSA.py/merge.py
+++ DSA.py/merge.py
@@ -1,24 +1,71 @@
-# arr = [5, 1, 4, 2]
+# # # arr = [5, 1, 4, 2]
 
-# n = len(arr)
+# # # n = len(arr)
 
-# for i in range(n):
-#     for j in range(0, n-i-1):
+# # # for i in range(n):
+# # #     for j in range(0, n-i-1):
 
-#         if arr[j] > arr[j+1]:
-#             arr[j], arr[j+1] = arr[j+1], arr[j]
+# # #         if arr[j] > arr[j+1]:
+# # #             arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# # # print(arr)
+# # arr = [64, 25, 12, 22]
+
+# # for i in range(len(arr)):
+
+# #     min_index = i
+
+# #     for j in range(i+1, len(arr)):
+# #         if arr[j] < arr[min_index]:
+# #             min_index = j
+
+# #     arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# # print(arr)
+# def merge_sort(arr):
+
+#     if len(arr) > 1:
+
+#         mid = len(arr)//2
+
+#         left = arr[:mid]
+#         right = arr[mid:]
+
+#         merge_sort(left)
+#         merge_sort(right)
+
+#         i = j = k = 0
+
+#         while i < len(left) and j < len(right):
+
+#             if left[i] < right[j]:
+#                 arr[k] = left[i]
+#                 i += 1
+#             else:
+#                 arr[k] = right[j]
+#                 j += 1
+
+#             k += 1
+
+#         while i < len(left):
+#             arr[k] = left[i]
+#             i += 1
+#             k += 1
+
+#         while j < len(right):
+#             arr[k] = right[j]
+#             j += 1
+#             k += 1
+
+# arr = [38, 27, 43, 3]
+
+# merge_sort(arr)
 
 # print(arr)
-arr = [64, 25, 12, 22]
+arr = [
+    [1, 2],
+    [3, 4]
+]
 
-for i in range(len(arr)):
-
-    min_index = i
-
-    for j in range(i+1, len(arr)):
-        if arr[j] < arr[min_index]:
-            min_index = j
-
-    arr[i], arr[min_index] = arr[min_index], arr[i]
-
-print(arr)
+for row in arr:
+    print(row)

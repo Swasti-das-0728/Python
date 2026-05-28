e1906cbe5173f97d5f381c93fdc3ffbebb439a82
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-28 22:19:15 +0530

    advance

--- DSA.py/merge.py
+++ DSA.py/merge.py
@@ -109,11 +109,20 @@
 # print(prefix)
 arr = [-2,1,-3,4,-1,2,1,-5,4]
 
-max_sum = arr[0]
-current = arr[0]
+# max_sum = arr[0]
+# current = arr[0]
 
-for i in range(1, len(arr)):
-    current = max(arr[i], current + arr[i])
-    max_sum = max(max_sum, current)
+# for i in range(1, len(arr)):
+#     current = max(arr[i], current + arr[i])
+#     max_sum = max(max_sum, current)
 
-print(max_sum)
+# print(max_sum)
+arr = [1, 2, 2, 3, 4, 4]
+
+dup = []
+
+for i in arr:
+    if arr.count(i) > 1 and i not in dup:
+        dup.append(i)
+
+print(dup)

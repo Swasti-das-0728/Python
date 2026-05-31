d2b7cfd5d583e0a23488bbf6a2d64606a5a4d994
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-05-31 23:03:50 +0530

    Initial commit

--- DSA.py/matrix.py
+++ DSA.py/matrix.py
@@ -1,10 +1,24 @@
-a = [[1,2],[3,4]]
-b = [[5,6],[7,8]]
+# a = [[1,2],[3,4]]
+# b = [[5,6],[7,8]]
 
-result = [[0,0],[0,0]]
+# result = [[0,0],[0,0]]
 
-for i in range(2):
-    for j in range(2):
-        result[i][j] = a[i][j] + b[i][j]
+# for i in range(2):
+#     for j in range(2):
+#         result[i][j] = a[i][j] + b[i][j]
 
-print(result)
+# print(result)
+a = [[1,2,3],
+     [4,5,6]]
+
+transpose = []
+
+for i in range(len(a[0])):
+    row = []
+
+    for j in range(len(a)):
+        row.append(a[j][i])
+
+    transpose.append(row)
+
+print(transpose)

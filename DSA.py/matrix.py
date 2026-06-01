e8a405c9451db44a4a8af3e876ed25dfecfe379c
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-01 22:41:33 +0530

    Initial commit

--- DSA.py/matrix.py
+++ DSA.py/matrix.py
@@ -7,18 +7,34 @@
 #     for j in range(2):
 #         result[i][j] = a[i][j] + b[i][j]
 
-# print(result)
-a = [[1,2,3],
-     [4,5,6]]
+# # print(result)
+# a = [[1,2,3],
+#      [4,5,6]]
 
-transpose = []
+# transpose = []
 
-for i in range(len(a[0])):
-    row = []
+# for i in range(len(a[0])):
+#     row = []
 
-    for j in range(len(a)):
-        row.append(a[j][i])
+#     for j in range(len(a)):
+#         row.append(a[j][i])
 
-    transpose.append(row)
+#     transpose.append(row)
 
-print(transpose)
+# print(transpose)
+def quick_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr)//2]
+
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return left + middle + right
+
+arr = [10, 7, 8, 9, 1, 5]
+
+print(quick_sort(arr))

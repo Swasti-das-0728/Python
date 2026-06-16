1f122c4119c91a6e79e03630cd6394bea0c6fb4b
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-16 21:47:55 +0530

    updated

---
+++ DSA.py/logpal.py
@@ -0,0 +1,11 @@
+def count_digits(n):
+    count = 0
+
+    while n > 0:
+        count += 1
+        n //= 10
+
+    return count
+
+n = int(input("Enter a number: "))
+print("Number of digits:", count_digits(n))

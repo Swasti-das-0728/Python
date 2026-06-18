f806cc293bd7eb7ff387e814fd40f94d74b070c5
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-18 21:31:25 +0530

    updated

--- DSA.py/armstrong.py
+++ DSA.py/armstrong.py
@@ -1,13 +1,20 @@
-# armtrong number should be if 153 = 1*3 + 5*3 + 3*3 = 153
-n = 153
+# # armtrong number should be if 153 = 1*3 + 5*3 + 3*3 = 153
+# n = 153
+# num = n
+# total = 0
+# nod = len(str(n))
+# while num>0:
+#     ld = num % 10
+#     total = total + (ld ** nod)
+#     num = num // 10
+# print ( total == n , "yes the number is armstrong number")
+
+
+
+n = 23433
 num = n
-total = 0
-nod = len(str(n))
+count = 0
 while num>0:
-    ld = num % 10
-    total = total + (ld ** nod)
+    count+=1
     num = num // 10
-print ( total == n , "yes the number is armstrong number")
-
-
-
+print( count)

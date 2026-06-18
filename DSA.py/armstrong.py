f379473e13b5b77ca10104600f0acb5fc8d8a3a7
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-18 21:53:12 +0530

    updated

--- DSA.py/armstrong.py
+++ DSA.py/armstrong.py
@@ -11,10 +11,37 @@
 
 
 
-n = 23433
-num = n
-count = 0
-while num>0:
-    count+=1
-    num = num // 10
-print( count)
+# n = 23433
+# num = n
+# count = 0
+# while num>0:
+#     count+=1
+#     num = num // 10
+# print( count)
+
+# n = 153
+# num = n 
+# total = 0
+# nod = len(str(n))
+# while num > 0:
+#     ld = num % 10
+#     total = total + (ld ** nod)
+#     num = num // 10
+# print(total == n)
+def palindrome(n):
+    temp = n
+    rev = 0
+
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n //= 10
+
+    return temp == rev
+
+num = int(input("Enter a number: "))
+
+if palindrome(num):
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")

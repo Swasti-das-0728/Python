e0a35ba86d9cadaf26f61180cf5abe18e615bb56
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-19 21:24:57 +0530

    updated

--- DSA.py/factor.py
+++ DSA.py/factor.py
@@ -10,12 +10,22 @@
 
 
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
+# result = []
+
+# for i in range(1, num // 2 + 1):
+#     if num % i == 0:
+#         result.append(i)
+
+# result.append(num)
+# print(result)
+from math import sqrt
+num = int(input("enter a number:"))
 result = []
-
-for i in range(1, num // 2 + 1):
-    if num % i == 0:
+for i in range(1,int(sqrt(num))+1):
+    if num%i == 0:
         result.append(i)
-
-result.append(num)
+    if num // i != i:
+        result.append(num // i)
+result.sort()
 print(result)

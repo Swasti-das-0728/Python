9239ff88da6fe5130fb01a02e27734ee501ec2b9
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-29 00:40:28 +0530

    updated

--- DSA.py/interval.py
+++ DSA.py/interval.py
@@ -1,8 +1,21 @@
-a = 10
-b = 22
-print(a*b)
-print(a%b)
-print(a^b)
-print(a-b)
-print(a+b)
-print(a/b)
+# # a = 10
+# # b = 22
+# print(a*b)
+# print(a%b)
+# print(a^b)
+# print(a-b)
+# print(a+b)
+# print(a/b)
+# print( 343 ==  234 and 34>43)
+
+# job = int(input("please give me the cgpa :- "))
+# if job == 7:
+#     print("i will accept the job")
+# else:
+#     print("i don't need this")
+
+money = int(input("enter the money :-"))
+if money == 21:
+    print("hat")
+else:
+    print("no")

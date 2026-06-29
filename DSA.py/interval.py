942220bf98700da3b1730fd1aca47187f123f9b3
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-06-29 13:47:27 +0530

    updated

--- DSA.py/interval.py
+++ DSA.py/interval.py
@@ -14,8 +14,14 @@
 # else:
 #     print("i don't need this")
 
-money = int(input("enter the money :-"))
-if money == 21:
-    print("hat")
+# money = int(input("enter the money :-"))
+# if money == 21:
+#     print("hat")
+# else:
+#     print("no")
+str1 = str(input("enter a string"))
+str2 = str(input("enter a string"))
+if len(str1) > len(str2):
+    print("str1 is greater one")
 else:
-    print("no")
+    print("last one is bigger") 

a0fcd520437c207d3d29dcb6b1e49bac967795de
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-07-03 23:59:49 +0530

    updated

--- DSA.py/interval.py
+++ DSA.py/interval.py
@@ -19,60 +19,100 @@
 #     print("hat")
 # else:
 #     print("no")
-# str1 = str(input("enter a string"))
-# str2 = str(input("enter a string"))
-# if len(str1) > len(str2):
-#     print("str1 is greater one")
-# else:
-#     print("last one is bigger") 
-# position = input("please tell me your roll in a game")
-# if position == 'rusher':
-#     print("practice atleast 5hr")
-# else:
-#     print("go with the flow")
+# # str1 = str(input("enter a string"))
+# # str2 = str(input("enter a string"))
+# # if len(str1) > len(str2):
+# #     print("str1 is greater one")
+# # else:
+# #     print("last one is bigger") 
+# # position = input("please tell me your roll in a game")
+# # if position == 'rusher':
+# #     print("practice atleast 5hr")
+# # else:
+# #     print("go with the flow")
 
-# num = int(input("provide your number"))
-# if num % 2 == 0 :
-#     print("it is a even number")
-# else:
-# #     print("odd")
-# name = input("enter your name")
-# age = int(input("enter the age"))
+# # num = int(input("provide your number"))
+# # if num % 2 == 0 :
+# #     print("it is a even number")
+# # else:
+# # #     print("odd")
+# # name = input("enter your name")
+# # age = int(input("enter the age"))
 
-# if age> 18:
-#     print("hello + {name} you are a valid voter")
-# else:
-#     print("wait till 18")
+# # if age> 18:
+# #     print("hello + {name} you are a valid voter")
+# # else:
+# #     print("wait till 18")
 
-# num = int(input("enter the table number"))
-# for i in range(num,(num*10+1),num):
-#     print(i)
-# a = "swasti"
-# for i in range(len(a)):
-#     print(a[i])
-# a = "teg thryuy erdfuf"
-# for i in  a:
-#     if i == 'y':
-#         break
-#     else:
-#         print(i)
+# # num = int(input("enter the table number"))
+# # for i in range(num,(num*10+1),num):
+# #     print(i)
+# # a = "swasti"
+# # for i in range(len(a)):
+# #     print(a[i])
+# # a = "teg thryuy erdfuf"
+# # for i in  a:
+# #     if i == 'y':
+# #         break
+# #     else:
+# #         print(i)
 
-def second_largest(arr):
+# # def second_largest(arr):
 
-    first = second = float('-inf')
+# #     first = second = float('-inf')
 
-    for num in arr:
+# #     for num in arr:
 
-        if num > first:
-            second = first
-            first = num
+# #         if num > first:
+# #             second = first
+# #             first = num
 
-        elif num > second and num != first:
-            second = num
+# #         elif num > second and num != first:
+# #             second = num
 
-    return second
+# #     return second
 
 
-arr = list(map(int, input("Enter array: ").split()))
+# # arr = list(map(int, input("Enter array: ").split()))
 
-print(second_largest(arr))
+# # print(second_largest(arr))
+# def pair_sum(arr, target):
+
+#     seen = set()
+
+#     for num in arr:
+
+#         if target - num in seen:
+#             return True
+
+#         seen.add(num)
+
+#     return False
+
+
+# arr = list(map(int, input().split()))
+
+# target = int(input("Target: "))
+
+# # print(pair_sum(arr, target))
+# def frequency(arr):
+
+#     freq = {}
+
+#     for i in arr:
+
+#         if i in freq:
+#             freq[i] += 1
+#         else:
+#             freq[i] = 1
+
+#     return freq
+
+
+# arr = list(map(int, input().split()))
+
+# print(frequency(arr))
+print("Hello World")
+
+name = input("Enter your name: ")
+print("Welcome", name)

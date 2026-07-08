7d7cdd79db29b0ebf3db017a0b3670ac69b0794c
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-07-09 00:19:29 +0530

    updated

--- DSA.py/interval.py
+++ DSA.py/interval.py
@@ -191,12 +191,16 @@
 # print(sum)
         
        
-n = int(input("Enter a number: "))
-prime=0
-for i in range(1,n+1):
-    if n%i == 0:
-        prime = prime + 1
-print(prime)
-if prime<=2:
-    print("this is a prime number")
+# n = int(input("Enter a number: "))
+# prime=0
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         prime = prime + 1
+# print(prime)
+# if prime<=2:
+#     print("this is a prime number")
          
+
+a = "swasti"
+for i in range(len(a)-1,-1,-1):
+    print(a[i])

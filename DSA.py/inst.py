241d914d52563b3873daa0ed28d7c5f08c0e3000
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-08-04 07:52:24 +0530

    updated

--- DSA.py/inst.py
+++ DSA.py/inst.py
@@ -1,14 +1,24 @@
-arr = [12, 11, 13, 5]
+# arr = [12, 11, 13, 5]
 
-for i in range(1, len(arr)):
+# for i in range(1, len(arr)):
 
-    key = arr[i]
-    j = i - 1
+#     key = arr[i]
+#     j = i - 1
 
-    while j >= 0 and key < arr[j]:
-        arr[j + 1] = arr[j]
-        j -= 1
+#     while j >= 0 and key < arr[j]:
+#         arr[j + 1] = arr[j]
+#         j -= 1
 
-    arr[j + 1] = key
+#     arr[j + 1] = key
 
-print(arr)
+# print(arr)
+a = str(input("enter the string"))
+b = ""
+
+for i in range (len(a) -1,-1,-1):
+    b = b + a[i]
+if b == a:
+    print(" paleindrom")
+else:
+    print("not")
+
---
+++ kuna.py
@@ -0,0 +1,1 @@
+nahi chota hai nunna

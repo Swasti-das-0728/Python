d00d4c847195e0ed1b75354f0c6807faac523668
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-08-22 23:56:19 +0530

    updated

--- DSA.py/inhertance.py
+++ DSA.py/inhertance.py
@@ -1,9 +1,24 @@
-class India:
-    cm = "narendraaaaa"
-    def budget(self):
-        print("200cr")
-class Odisha(India):
-    pass
-obj = Odisha()
-obj.budget()
-print( obj.cm )
+# class India:
+#     cm = "narendraaaaa"
+#     def budget(self):
+#         print("200cr")
+# class Odisha(India):
+#     pass
+# obj = Odisha()
+# obj.budget()
+# print( obj.cm )
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+
+        return original == reverse

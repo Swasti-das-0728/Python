955959ed59753932bdf94150d06aed633c145e34
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-08-24 10:50:38 +0530

    updated

--- DSA.py/inhertance.py
+++ DSA.py/inhertance.py
@@ -8,17 +8,29 @@
 # obj.budget()
 # print( obj.cm )
 
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0:
+#             return False
 
-        original = x
-        reverse = 0
+#         original = x
+#         reverse = 0
 
-        while x > 0:
-            digit = x % 10
-            reverse = reverse * 10 + digit
-            x = x // 10
+#         while x > 0:
+#             digit = x % 10
+#             reverse = reverse * 10 + digit
+#             x = x // 10
 
-        return original == reverse
+#         return original == reverse
+
+
+class Animal:
+    def __init__(self,name):
+        self.name = name
+    def show(self):
+        print(f"hello name is {self.name}")
+class Human(Animal):
+    pass
+
+person  = Human("Swasti")
+person.show()

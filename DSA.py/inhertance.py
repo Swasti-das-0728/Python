53e4442b5a261aa069c4cbc0def5837e213113b5
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-08-26 23:53:49 +0530

    updated

--- DSA.py/inhertance.py
+++ DSA.py/inhertance.py
@@ -24,13 +24,63 @@
 #         return original == reverse
 
 
-class Animal:
-    def __init__(self,name):
-        self.name = name
-    def show(self):
-        print(f"hello name is {self.name}")
-class Human(Animal):
-    pass
+# class Animal:
+#     def __init__(self,name):
+#         self.name = name
+#     def show(self):
+#         print(f"hello name is {self.name},")
+# class Human(Animal):
+#     def __init__(self, name,age):
+#         super().__init__(name)
+#         self.age = age
+#     def show(self):
+#         print(f"hello name is {self.name},{self.age}")
+#     pass
 
-person  = Human("Swasti")
-person.show()
+# animal = Animal("lion")
+# person = Human("swasti",82)
+
+# person.show()
+
+# class School:
+#     def __init__(self,name):
+#         self.name = name
+        
+#         print(f"hello there {self.name}")
+
+    
+
+# class Tuition:
+#     def __init__(self,name,address):
+#         self.name = name
+#         self.address = address
+#         print(f"hello there name is {self.name},{self.address}")
+# class Teacher(Tuition,School):
+#     pass
+
+# sdd = School("swasti",34)
+
+# School.show()
+    
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = "1"
+
+        for _ in range(n - 1):
+            i = 0
+            ans = ""
+
+            while i < len(s):
+                count = 1
+
+                while i + 1 < len(s) and s[i] == s[i + 1]:
+                    count += 1
+                    i += 1
+
+                ans += str(count) + s[i]
+                i += 1
+
+            s = ans
+
+        return s

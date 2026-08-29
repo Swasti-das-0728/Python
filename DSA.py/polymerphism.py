9fc4222b5690bfeea87f99f591f0a6ced212d1d8
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-08-29 11:23:51 +0530

    updated

--- DSA.py/polymerphism.py
+++ DSA.py/polymerphism.py
@@ -1,11 +1,24 @@
-class Animal:
-    def show2(self):
-        print("hello this is swasti")
-class Human(Animal):
+# class Animal:
+#     def show2(self):
+#         print("hello this is swasti")
+# class Human(Animal):
+#     def show(self):
+#         print("how are you")
+
+
+# obj = Human()
+# obj.show2()
+        
+
+# Docktyping
+
+class Swasti:
     def show(self):
-        print("how are you")
-
-
-obj = Human()
-obj.show2()
-         
+        print("hi")
+class Das:
+    def show(self):
+        print("hello")
+obj = Swasti()
+obj2 = Das()
+obj.show()
+obj2.show()

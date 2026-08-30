6ff8421899223d73172c5b2fe679db4553b935d8
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-08-30 23:49:35 +0530

    updated

--- DSA.py/encapsulation.py
+++ DSA.py/encapsulation.py
@@ -10,12 +10,23 @@
 # #         print(super().a)
 # # obj = Hello()
 # # obj.show2()
-class Student:
-    def __init__(self, name):
-        self.name = name
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
 
-    def display(self):
-        print("Name:", self.name)
+#     def display(self):
+#         print("Name:", self.name)
 
-s1 = Student("Swastii")
-s1.display()
+# s1 = Student("Swastii")
+# # s1.display()
+# class Parent:
+#     def __init__(self):
+#         print("Parent Constructor")
+
+# class Child(Parent):
+#     def __init__(self):
+#         super().__init__()
+#         print("Child Constructor")
+
+# c = Child()
+

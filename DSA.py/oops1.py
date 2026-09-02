d10d8353bb804bf99b92b3c71063d32c73a72285
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-09-02 23:36:13 +0530

    updated

--- DSA.py/oops1.py
+++ DSA.py/oops1.py
@@ -16,12 +16,35 @@
 # d.show()
 
 # print(D.mro())
-class Bank:
-    def __init__(self):
-        self.__balance = 1000
+# class Bank:
+#     def __init__(self):
+#         self.__balance = 1000
 
-    def get_balance(self):
-        return self.__balance
+#     def get_balance(self):
+#         return self.__balance
 
-b = Bank()
-print(b.get_balance())
+# b = Bank()
+# print(b.get_balance())
+
+# class Parent:
+#     def __init__(self):
+#         print("Parent")
+
+# class Child(Parent):
+#     pass
+
+# Child()
+
+class Student:
+    college = "MIT"
+
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("Alice")
+s2 = Student("Bob")
+
+Student.college = "Harvard"
+
+print(s1.college)
+print(s2.college)

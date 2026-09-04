98c85ac9c736798094d653ead9f0555649abd799
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-09-04 23:53:15 +0530

    updated

--- DSA.py/oops1.py
+++ DSA.py/oops1.py
@@ -15,55 +15,67 @@
 # d = D()
 # d.show()
 
-# print(D.mro())
-# class Bank:
-#     def __init__(self):
-#         self.__balance = 1000
+# # print(D.mro())
+# # class Bank:
+# #     def __init__(self):
+# #         self.__balance = 1000
 
-#     def get_balance(self):
-#         return self.__balance
+# #     def get_balance(self):
+# #         return self.__balance
 
-# b = Bank()
-# print(b.get_balance())
+# # b = Bank()
+# # print(b.get_balance())
 
-# class Parent:
-#     def __init__(self):
-#         print("Parent")
+# # class Parent:
+# #     def __init__(self):
+# #         print("Parent")
 
-# class Child(Parent):
-#     pass
+# # class Child(Parent):
+# #     pass
 
-# # Child()
+# # # Child()
 
-# class Student:
-#     college = "MIT"
+# # class Student:
+# #     college = "MIT"
 
-#     def __init__(self, name):
-#         self.name = name
+# #     def __init__(self, name):
+# #         self.name = name
 
-# s1 = Student("Alice")
-# s2 = Student("Bob")
+# # s1 = Student("Alice")
+# # s2 = Student("Bob")
 
-# Student.college = "Harvard"
+# # Student.college = "Harvard"
 
-# print(s1.college)
-# print(s2.college)
+# # print(s1.college)
+# # print(s2.college)
 
-n = 5
-s = "1"
+# n = 5
+# s = "1"
 
-for _ in range(n - 1):
-    ans = ""
-    count = 1
+# for _ in range(n - 1):
+#     ans = ""
+#     count = 1
 
-    for i in range(1, len(s)):
-        if s[i] == s[i - 1]:
-            count += 1
-        else:
-            ans += str(count) + s[i - 1]
-            count = 1
+#     for i in range(1, len(s)):
+#         if s[i] == s[i - 1]:
+#             count += 1
+#         else:
+#             ans += str(count) + s[i - 1]
+#             count = 1
 
-    ans += str(count) + s[-1]
-    s = ans
+#     ans += str(count) + s[-1]
+#     s = ans
 
-print(s)
+# print(s)
+
+
+from itertools import groupby
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = "1"
+
+        for _ in range(n - 1):
+            s = "".join(str(len(list(g))) + k for k, g in groupby(s))
+
+        return s

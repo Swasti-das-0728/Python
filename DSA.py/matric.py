a35a83ce58d9e79e1a6a2e900e0fb60949e8f14c
Swasti Deepan Das <srikantdad1967@gmail.com>
2026-09-09 23:57:19 +0530

    updated

--- DSA.py/matric.py
+++ DSA.py/matric.py
@@ -1,22 +1,38 @@
-class Solution:
-    def countAndSay(self, n: int) -> str:
-        s = "1"
+# class Solution:
+#     def countAndSay(self, n: int) -> str:
+#         s = "1"
 
-        for _ in range(n - 1):
-            ans = ""
-            i = 0
+#         for _ in range(n - 1):
+#             ans = ""
+#             i = 0
 
-            while i < len(s):
-                count = 1
+#             while i < len(s):
+#                 count = 1
 
-                while i < len(s) - 1 and s[i] == s[i + 1]:
-                    count += 1
-                    i += 1
+#                 while i < len(s) - 1 and s[i] == s[i + 1]:
+#                     count += 1
+#                     i += 1
 
-                ans += str(count)
-                ans += s[i]
-                i += 1
+#                 ans += str(count)
+#                 ans += s[i]
+#                 i += 1
 
-            s = ans
+#             s = ans
 
-        return s
+#         return s
+class Dog:
+    def sound(self):
+        print("Bark")
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+class Cow:
+    def sound(self):
+        print("Moo")
+
+animals = [Dog(), Cat(), Cow()]
+
+for animal in animals:
+    animal.sound()
